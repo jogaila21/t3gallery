59a80b781f5b509ef57750f07c3607d1f9b9c999
jogaila21 <najarfadi50@gmail.com>
2025-06-13 15:47:56 -0400

    Compatibility issues, downgraded to previous version

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'next/core-web-vitals',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: ['.next', 'dist', 'node_modules'],
+};
